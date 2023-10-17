89350f7172103b9abff524fdb0852692cc394811
Hey-Claudia <chclaudiahesse@gmail.com>
2023-10-17 17:08:37 +0200

    Fixed JS for challenges

---
+++ challenge1.js
@@ -0,0 +1,22 @@
+/* This is the firstname of the user */
+const user = 'John'
+
+/* This is the lastname of the user */
+const surname = 'Smith'
+
+console.log(user, surname);
+
+
+
+
+
+
+// /* This is the firstname of the user /*
+
+// const user = 'John'
+
+
+// /* This is the lastname of the user /* const surname = 'Smith'
+
+
+// console.log(user; surname;)
---
+++ challenge2.js
@@ -0,0 +1,22 @@
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date)
+
+
+
+
+
+
+// /** (c) ACME Inc. 2010 */
+
+// /*
+//  * This is the date that a user created their account 
+//  */
+// const date = '10/07/2014'
+
+// console(date)
